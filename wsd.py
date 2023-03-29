ae0e7ff285fae5e16c30a941aa151dd677b9add8
David <davferrufino11@gmail.com>
2023-03-29 10:39:30 -0400

    Initial commit

---
+++ wsd.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+from sys import argv
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+#getting i/o locations
+train = argv[1]
+test = argv[2]
+model = argv[3]
+answers = argv[4]
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
