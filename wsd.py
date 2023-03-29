9179028282326b62065f0326f34bdcd36f793ac7
David <davferrufino11@gmail.com>
2023-03-29 15:46:49 -0400

    project now gets senses and uses certain words for rules. Need to set up rules and fix issue where each rule is only counted one time

--- wsd.py
+++ wsd.py
@@ -10,6 +10,77 @@ test = argv[2]
 model = argv[3]
 answers = argv[4]
 
+#my features for line
+features = "call","device","machine","sales","launch","distance","unit","production","sale"
+#So first I am trying to make sense based on features
+#So the sense would be phone, and feature would be line
+
+
+#line by line
+Sense = []
+key = ""
+
+f =  open(train, "r")
+with open(train) as file:
+    for line in file:
+        #storing what the correct sense is
+        if("senseid" in line):
+            if("phone" in line):
+                key = "phone"
+            if("product" in line):
+                key = "product"
+            #going to use the senseid as the key in the dic
+        if("<s>" in line):
+            aFeatures = [0,0,0,0,0,0,0,0,0, key]
+            for i in range(len(features)-1):
+                if(features[i] in line):
+                    aFeatures[i] = aFeatures[i]+1
+                    continue
+
+            aFeaturesTuple = tuple(aFeatures)
+            Sense.append(aFeatures)
+
+for vector in Sense:
+    if(vector[0] > 0):
+        #Now the last index of Sense will have the sense determined by my test
+        vector.append("phone")
+    else:
+        if (vector[1] > 0):
+            # return
+            vector.append("product")
+        else:
+            if (vector[2] > 0):
+                # return
+                vector.append("product")
+            else:
+                if (vector[3] > 0):
+                    # return
+                    vector.append("product")
+                else:
+                    if (vector[4] > 0):
+                        # return
+                        vector.append("product")
+                    else:
+                        if (vector[5] > 0):
+                            # return
+                            vector.append("phone")
+                        else:
+                            if (vector[6] > 0):
+                                # return
+                                vector.append("product")
+                            else:
+                                if (vector[7] > 0):
+                                    # return
+                                    vector.append("product")
+                                else:
+                                    if (vector[8] > 0):
+                                        # return
+                                        vector.append("product")
+                                    else:
+                                        vector.append("product")
+
+
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
